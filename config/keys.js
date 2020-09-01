c0dfd37cc79b370e62b7922cdd27935fc6083b1d
Rajeev Ranjan <rjeevrnjn17@gmail.com>
2020-09-01 19:19:56 +0530

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+  "mongodb+srv://eviilraj:projectcrypto@cluster0-xrtws.mongodb.net/twin-auth?retryWrites=true&w=majority"  ,secretOrKey: "secret",
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/users");
+const pandits = require("./routes/pandits");
+
+const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
+
+// Use Routes
+app.use("/users", users);
+app.use("/pandits", pandits);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
